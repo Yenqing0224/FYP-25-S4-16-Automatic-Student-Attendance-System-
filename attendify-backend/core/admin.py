19e9b3e5707271cbfbfbd20bda25d324b0b2e3cf
Yenqing0224 <syenqing0224@gmail.com>
2025-12-06 18:56:13 +0800

    allow more field when creating new user on admin panel

--- attendify-backend/core/admin.py
+++ attendify-backend/core/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Admin, Lecturer, Student
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('phone_number', 'role_type', 'gender', 'address')}),
+    )
+    
+    # This allows these fields to be filled in when creating a NEW user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'role_type')}),
+    )
+
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Admin)
 admin.site.register(Lecturer)
 admin.site.register(Student)
