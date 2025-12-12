4fefc62694c1c08fcb51459aa8e9d23ba29b9fbe
Yenqing0224 <syenqing0224@gmail.com>
2025-12-12 15:33:12 +0800

    Update user views API

--- attendify-backend/core/interface/views/users_views.py
+++ attendify-backend/core/interface/views/users_views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 # Import Serializers
@@ -8,33 +8,24 @@ from core.models import User, Student
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def get_student_profile(request):
     try:
-        user_id = request.query_params.get('id')
-        if not user_id:
-            return Response({"error": "User ID required"}, status=400)
-
-        student = Student.objects.get(user__id=user_id)
+        student = Student.objects.get(user=request.user)
         
         return Response(StudentSerializer(student).data)
 
     except Student.DoesNotExist:
-        return Response({"error": "Student not found"}, status=404)
+        return Response({"error": "Student profile not found"}, status=404)
     except Exception as e:
         return Response({"error": str(e)}, status=500)
     
 
 @api_view(['PATCH'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def edit_profile(request):
     try:
-        user_id = request.data.get('user_id')
-        
-        if not user_id:
-            return Response({"error": "User ID is required"}, status=400)
-
-        user = User.objects.get(id=user_id)
+        user = request.user 
 
         user.phone_number = request.data.get('phone_number', user.phone_number)
         user.personal_email = request.data.get('personal_email', user.personal_email)
@@ -47,8 +38,6 @@ def edit_profile(request):
 
         return Response({"message": "Profile updated successfully"}, status=200)
 
-    except User.DoesNotExist:
-        return Response({"error": "User not found"}, status=404)
     except Exception as e:
         print(f"Update Profile Error: {e}")
         return Response({"error": str(e)}, status=500)
