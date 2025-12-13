54ec863730098610843c8468038e9fdb71f16980
Yenqing0224 <syenqing0224@gmail.com>
2025-12-13 21:21:26 +0800

    Update admin_serializers.py

--- attendify-backend/core/interface/serializers/admin_serializers.py
+++ attendify-backend/core/interface/serializers/admin_serializers.py
@@ -4,8 +4,6 @@ from core.models import *
 
 # Users
 class AdminUserSerializer(serializers.ModelSerializer):
-    is_staff = serializers.BooleanField(default=False)
-
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email', 
@@ -17,6 +15,14 @@ class AdminUserSerializer(serializers.ModelSerializer):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
         
+        raw_is_staff = self.initial_data.get('is_staff')
+        
+        if raw_is_staff is not None:
+            if str(raw_is_staff).lower() == 'true':
+                instance.is_staff = True
+            else:
+                instance.is_staff = False
+
         if password is not None:
             instance.set_password(password)
             
@@ -29,6 +35,14 @@ class AdminUserSerializer(serializers.ModelSerializer):
         
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
+
+        raw_is_staff = self.initial_data.get('is_staff')
+
+        if raw_is_staff is not None:
+            if str(raw_is_staff).lower() == 'true':
+                instance.is_staff = True
+            else:
+                instance.is_staff = False
 
         if password is not None:
             instance.set_password(password)
