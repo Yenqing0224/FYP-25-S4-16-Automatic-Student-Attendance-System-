94a479d2117b581b37368f8da818e05033b091f5
Yenqing0224 <syenqing0224@gmail.com>
2026-01-11 18:20:18 +0800

    Updated edit profile structure

--- attendify-backend/core/interface/views/users_views.py
+++ attendify-backend/core/interface/views/users_views.py
@@ -40,20 +40,15 @@ def get_profile(request):
 @api_view(['PATCH'])
 @permission_classes([IsAuthenticated])
 def edit_profile(request):
+    service = UserService()
     try:
-        user = request.user 
+        message = service.edit_profile(request.user, request.data)
 
-        user.phone_number = request.data.get('phone_number', user.phone_number)
-        user.personal_email = request.data.get('personal_email', user.personal_email)
-        user.address_country = request.data.get('address_country', user.address_country)
-        user.address_street = request.data.get('address_street', user.address_street)
-        user.address_unit = request.data.get('address_unit', user.address_unit)
-        user.address_postal = request.data.get('address_postal', user.address_postal)
-
-        user.save()
-
-        return Response({"message": "Profile updated successfully"}, status=200)
-
+        return Response({"message": message}, status=200)
+    
+    except ValueError as e:
+        return Response({"error": str(e)}, status=400)
+    
     except Exception as e:
         print(f"Update Profile Error: {e}")
         return Response({"error": str(e)}, status=500)
--- attendify-backend/core/services/users_services.py
+++ attendify-backend/core/services/users_services.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from core.logic.users_logics import UserLogic
 from core.models import Student, Lecturer
 
 
@@ -11,3 +12,26 @@ class UserService:
             return Lecturer.objects.get(user=user)
         else:
             raise ValueError(f"No profile defined for role: {user.role_type}")
+        
+    
+    def edit_profile(self, user, data):
+        is_valid, message = UserLogic.validate_update_data(data)
+        if not is_valid:
+            return ValueError(message)
+        
+        editable_field = ['phone_number', 'personal_email', 'address_country', 
+                          'address_street', 'address_unit', 'address_postal']
+        
+        updated_count = 0
+
+        for x in editable_field:
+            if x in data:
+                setattr(user, x, data[x])
+                updated_count += 1
+        
+        if updated_count > 0:
+            user.save()
+            return f"Successfully updated {updated_count} fields."
+        else:
+            return "No changes made."
+
