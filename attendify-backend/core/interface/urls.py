47ed4d139ff3b96309ee300e7d39e1e414fda63c
Yenqing0224 <syenqing0224@gmail.com>
2026-01-14 23:44:23 +0800

    Update urls.py

--- attendify-backend/core/interface/urls.py
+++ attendify-backend/core/interface/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     path('timetable/', academics_views.get_timetable, name='timetable'),
     path('class-details/<int:session_id>/', academics_views.get_class_details, name='class-details'),
     path('attendance-history/', academics_views.get_attendance_history, name='attendance-history'),
-    path('recognize/', academics_views.recognize_face, name='recognize_face'),
-    path('register-face/', academics_views.register_face, name='register_face'),
+    # path('recognize/', academics_views.recognize_face, name='recognize_face'),
+    # path('register-face/', academics_views.register_face, name='register_face'),
 
     # Communication
     path('newsevent/', communication_views.get_newsevent, name='newsevent'),
