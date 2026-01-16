e66edae8e7ab0a6ed75576445ac4a4e2b4ad1cda
Yenqing0224 <syenqing0224@gmail.com>
2026-01-16 18:35:08 +0800

    Update setting..py

--- attendify-backend/backend_project/settings.py
+++ attendify-backend/backend_project/settings.py
@@ -96,6 +96,16 @@ DATABASES = {
     )
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
