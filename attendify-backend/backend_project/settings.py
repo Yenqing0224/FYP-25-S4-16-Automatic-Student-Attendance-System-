dbf2c188338b8159f0f78b002bd4cf448135026f
Yenqing0224 <syenqing0224@gmail.com>
2026-01-16 23:22:46 +0800

    Update settings.py

--- attendify-backend/backend_project/settings.py
+++ attendify-backend/backend_project/settings.py
@@ -110,7 +110,7 @@ CACHES = {
 # Outlook Email Setting
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('BREVO_LOGIN_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_KEY')
