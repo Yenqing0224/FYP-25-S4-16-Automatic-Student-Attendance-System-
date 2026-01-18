1dfebe4a8752c65b8b44b29d4fc0541a272750aa
Yenqing0224 <syenqing0224@gmail.com>
2026-01-18 21:48:42 +0800

    Update settings.py

--- attendify-backend/backend_project/settings.py
+++ attendify-backend/backend_project/settings.py
@@ -164,7 +164,7 @@ STORAGES = {
 AUTH_USER_MODEL = 'core.User'
 TIME_ZONE = 'Asia/Singapore'
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
@@ -173,11 +173,29 @@ CORS_ALLOWED_ORIGINS = [
     'https://attendify-app.netlify.app',
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://attendify-app.netlify.app",
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*--attendify-app\.netlify\.app$",
 ]
+
 
 SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://attendify-app.netlify.app",
+]
