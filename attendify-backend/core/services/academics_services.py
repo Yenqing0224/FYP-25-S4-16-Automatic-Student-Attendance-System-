d55bcd8301097e4b563e4de9d4750c9e556e6878
Yenqing0224 <syenqing0224@gmail.com>
2026-01-28 14:29:54 +0800

    Update academics_services.py

--- attendify-backend/core/services/academics_services.py
+++ attendify-backend/core/services/academics_services.py
@@ -410,8 +410,10 @@ class AcademicService:
         attendance, created = AttendanceRecord.objects.get_or_create(
             session=active_session,
             student=student,
-            defaults={'status': 'absent',
-                'duration': 0}
+            defaults={
+                'status': 'absent',
+                'duration': 0
+            }
         )
 
         if attendance.entry_time is None:
